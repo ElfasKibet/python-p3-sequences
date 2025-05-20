db659ce934ef027afe086beb9294622bde513287
elfas <kibetelfas17@gmail.com>
2025-05-20 19:41:02 +0300

    Assignment completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    fibonacci = [0, 1]
+    while len(fibonacci) < n:
+        next_value = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_value)
+    print(fibonacci[:n])
